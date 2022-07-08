b394df1e2ec6f5e927bb3e073e6af68f48e7aff0
lhgergo <lefthandgergo@gmail.com>
2022-07-08 10:30:23 +0200

    Replacing parallel and parapply-based parallelization with furrrr

--- R/RunCommand.R
+++ R/RunCommand.R
@@ -5,10 +5,9 @@ RunCommand <- function(cmds, threads = 1, intern = F) {
   if(threads == 1) {
     lapply(cmds, function(cmd) {system(cmd, intern = intern)})
   } else if (threads > 1) {
-    cl <- makeCluster(threads)
-    registerDoParallel(cl)
-    out <- parLapply(cl, cmds, function(cmd) {system(cmd, intern = intern)})
-    stopCluster(cl)
+    future::plan("multisession", workers = threads)
+    out <- furrr::future_map(cmds, ~system(.x, intern = intern))
+    future:::ClusterRegistry("stop")
     return(out)
   } else {
     message("Number of threads cannot be smaller than 1.")
