c5b023faecfb498e9b0f1716d04f29f29f2ce774
lhgergo <lefthandgergo@gmail.com>
2022-07-08 10:29:37 +0200

    removing old codes from the R directory, moving them to R_old

--- R/LogIt.R
+++
@@ -1,6 +0,0 @@
-# LogIt
-# pastes a new entry in a logfile, marked with timestamp and function name causing the issue
-LogIt <- function(msg, funcname, file, action = "silent") {
-  write(paste0("# ", Sys.time(), " ", funcname, " ", action, "\n", msg), file = file, append = TRUE)
-}
-
