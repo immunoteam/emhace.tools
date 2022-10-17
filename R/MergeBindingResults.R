751502a73c2dbd3c0e9273d8e472e105f4c76224
lhgergo <lefthandgergo@gmail.com>
2022-10-17 23:04:43 +0200

    removing fun.aggregate part

--- R/MergeBindingResults.R
+++ R/MergeBindingResults.R
@@ -3,6 +3,5 @@
 # x: list, containing the binding matrices (wide binding result objects) to be merged
 
 MergeBindingResults <- function(x) {
-  reshape2::acast(data = reshape2::melt(x), Var1 ~ Var2, value.var = "value", fun.aggregate = sum)
+  reshape2::acast(data = reshape2::melt(x), Var1 ~ Var2, value.var = "value")
 }
-MergeBindingResults()
