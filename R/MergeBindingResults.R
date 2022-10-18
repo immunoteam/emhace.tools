4f2c062fd7714dbdd4a810546ea0f6074412e0c9
lhgergo <lefthandgergo@gmail.com>
2022-10-18 17:57:42 +0200

    changing aggreagte function from sum to mean

--- R/MergeBindingResults.R
+++ R/MergeBindingResults.R
@@ -3,5 +3,5 @@
 # x: list, containing the binding matrices (wide binding result objects) to be merged
 
 MergeBindingResults <- function(x) {
-  reshape2::acast(data = reshape2::melt(x), Var1 ~ Var2, value.var = "value")
+  reshape2::acast(data = reshape2::melt(x), Var1 ~ Var2, value.var = "value", fun.aggregate = sum)
 }
