699f3666c71ec53b6eec15d446e9d4bece9b8eef
lhgergo <lefthandgergo@gmail.com>
2023-05-18 15:40:57 +0200

    adding support for NetMHCpan 4.0 outputs

--- R/CollectBindingResults.R
+++ R/CollectBindingResults.R
@@ -1,10 +1,19 @@
 #' @export
-CollectBindingResults <- function(results, value_type = c("Score_EL", "Rank_EL", "Score_BA", "Rank_BA", "Aff_nm"), output_format = "long", hla_type = 1){
+CollectBindingResults <- function(results, value_type = c("Score_EL", "Rank_EL", "Score_BA", "Rank_BA", "Aff_nm"), output_format = "long", hla_type = 1, version_number = "4.1"){
   if(hla_type == 1) {
     datarow_identifier <- "PEPLIST "
     to_be_trimmed_wb <- " <= WB"
     to_be_trimmed_sb <- " <= SB"
-    coords <- c(2, 3, 4, 12, 13, 14, 15, 16)
+    if(version_number == "4.1") {
+      coords <- c(2, 3, 4, 12, 13, 14, 15, 16)
+      clnms_output <- c("allele", "peptide", "core", "Score_EL", "Rank_EL", "Score_BA", "Rank_BA", "Aff_nm")
+    } else if(version_number == "4.0") {
+      coords <- c(2, 3, 4, 12, 13, 14)
+      clnms_output <- c("allele", "peptide", "core", "Score_BA", "Aff_nm", "Rank_BA")
+    } else {
+      stop("Version number is not supported")
+    }
+    
   } else if (hla_type == 2) {
     datarow_identifier <- "Sequence "
     to_be_trimmed_wb <- " <=WB"
@@ -13,7 +22,7 @@ CollectBindingResults <- function(results, value_type = c("Score_EL", "Rank_EL",
   } else {
     stop("The value of hla_type can be either 1 or 2!")
   }
-  readin <- results[[1]][grep(datarow_identifier, results[[1]])] 
+  readin <- results[[1]][grep(datarow_identifier, results[[1]])]
   peplist <- sapply(readin, function(x) {unlist(strsplit(x, "\\s+"))[4]}, USE.NAMES = FALSE)
   
   # create long format
@@ -25,9 +34,9 @@ CollectBindingResults <- function(results, value_type = c("Score_EL", "Rank_EL",
     mat <- do.call(rbind, strsplit(x, "\\s+"))
     mat[, coords]
   })), stringsAsFactors = F)
-  for (i in 4:8) {resultsdf[, i] <- as.numeric(resultsdf[, i])}
+  for (i in 4:ncol(resultsdf)) {resultsdf[, i] <- as.numeric(resultsdf[, i])}
   
-  colnames(resultsdf) <- c("allele", "peptide", "core", "Score_EL", "Rank_EL", "Score_BA", "Rank_BA", "Aff_nm")
+  colnames(resultsdf) <- clnms_output
   resultsdf <- cbind.data.frame(resultsdf[, 1:3], resultsdf[, colnames(resultsdf) %in% value_type, drop = FALSE])
   
   # create wide format
